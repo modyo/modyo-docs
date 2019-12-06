51fa1aabdf7adfc72b7696fdc706daaeaaf606e5
Ivan Valdes <ivaldes@modyo.com>
2019-12-06 10:23:33 -0300

    Translation support completion #MODYO-Docs

--- translator.rb
+++ translator.rb
@@ -1,43 +1,37 @@
 #!/usr/bin/env ruby
 #
-# Use AWS Translator to keep all the English definitions up to date.
-# Requires Ruby > 2.3 
+# Use Google Translator to keep all the English definitions up to date.
+# Requires Ruby > 2.3
 #
 # Install instructions:
 # - gem install google-cloud-translate
-# - gem install redcarpet
-#
-# TODO:
-# - Setup the Google Key for translation
-# - Save the translated files
+# - gem install kramdown
 #
 
-require 'redcarpet'
+# require 'kramdown'
 require 'fileutils'
 require "google/cloud/translate"
 
-locales = ['en']
+ENV["TRANSLATE_PROJECT"] = "modyo-cloud"
+ENV["TRANSLATE_CREDENTIALS"] = "google_cloud_credentials.json"
 
-Google::Cloud::Translate.configure do |config|
-    config.project_id  = ""
-    config.credentials = ""
+unless File.file? ENV["TRANSLATE_CREDENTIALS"]
+    raise LoadError, 'No Google Cloud Translate credentials file found, please include it as google_cloud_credentials.json'
 end
+
+locales = ['en']
 
 source_files = Dir.glob("docs/guides/**/*.md").map {|file| file.gsub("docs/","")}
 
 locales.each do |locale|
-    
     source_files.each do |source_file|
-        if File.exist?("docs/#{locale}/#{source_file}")
-            puts "#{source_file} Exists!"
-        else
+        unless File.exist?("docs/#{locale}/#{source_file}")
             puts "#{source_file} Not found :("
             puts "Creating #{source_file}"
             puts "Copying #{source_file}"
             FileUtils.cp("docs/#{source_file}", "docs/#{locale}/#{source_file}")
         end
     end
-    
     files = Dir.glob("docs/#{locale}/guides/**/*.md").map {|file| file.gsub("docs/#{locale}/","")}
 
     puts "Checking for orphan files ..."
@@ -52,17 +46,26 @@ locales.each do |locale|
     end
 
     puts "Reviewing translation status ..."
+    translate = Google::Cloud::Translate.new version: :v2
     files.each do |file|
-        unless File.open("docs/#{locale}/#{file}") =~ /translated: true/
+        unless File.readlines("docs/#{locale}/#{file}", encoding: 'utf-8').grep(/translated: true/).any?
             puts "#{file} requires auto translation!"
+            input_file = File.read("docs/#{locale}/#{file}", encoding: 'utf-8')
 
-            text = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(File.open("docs/#{locale}/#{file}", "r").read)
+            ## directly from Google Cloud Translate
+            translated = translate.translate input_file, to: "en", format: 'text'
+            output = translated.text
+            #TODO: El issue con caracteres especiales: https://issuetracker.google.com/issues/119256504
 
-            puts text
-            #translate = Google::Cloud::Translate.new
-            #translation = translate.translate File.open("docs/#{locale}/#{file}"), to: "en"
-            
-            #puts translation
+            ## Parsing with Kramdown
+            # input = Kramdown::Document.new(input_file).to_html
+            # translated = translate.translate input, to: "en", format: "html"
+            # output = Kramdown::Document.new(translated.text, input: 'html').to_kramdown
+
+            ## Writing to translated file
+            file = File.open("docs/#{locale}/#{file}", "w")
+            file.puts output
+            file.close
         end
     end
 end
