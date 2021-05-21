857b77b2758ae4b37e3ecad982d2e52ea73b65ac
Toño Silva Portell <jsilva@modyo.com>
2021-05-21 12:13:47 -0400

    Delete .idea configurations

--- translator.rb
+++ translator.rb
@@ -11,15 +11,15 @@ require 'fileutils'
 require "google/cloud/translate"
 
 ENV["TRANSLATE_PROJECT"] = "modyo-cloud"
-ENV["TRANSLATE_CREDENTIALS"] = "google_cloud_credentials.json"
+#ENV["TRANSLATE_CREDENTIALS"] = "google_cloud_credentials.json"
 
-unless File.file? ENV["TRANSLATE_CREDENTIALS"]
-    raise LoadError, 'No Google Cloud Translate credentials file found, please include it as google_cloud_credentials.json'
-end
+#unless File.file? ENV["TRANSLATE_CREDENTIALS"]
+#    raise LoadError, 'No Google Cloud Translate credentials file found, please include it as google_cloud_credentials.json'
+#end
 
 doc_modules = ['platform','widgets','legal','coding','brand']
 master_locale = ['es']
-locales = ['es']
+locales = ['en']
 
 
 source_files = Dir.glob("docs/platform/**/*.md").map {|file| file.gsub("docs/","")}
@@ -49,35 +49,35 @@ locales.each do |locale|
         File.delete("docs/#{locale}/#{orphan_file}") if File.exist?("docs/#{locale}/#{orphan_file}")
     end
 
-    puts "Reviewing translation status ..."
-    client = Google::Cloud::Translate.new
-    parent = client.class.location_path ENV["TRANSLATE_PROJECT"], "global"
-    files.each do |file|
-        unless File.readlines("docs/#{locale}/#{file}", encoding: 'utf-8').grep(/translated: true/).any?
-            puts "#{file} requires auto translation!"
-            input_file = File.read("docs/#{file}", encoding: 'utf-8')
+    # puts "Reviewing translation status ..."
+    # client = Google::Cloud::Translate.new
+    # parent = client.class.location_path ENV["TRANSLATE_PROJECT"], "global"
+    # files.each do |file|
+    #     unless File.readlines("docs/#{locale}/#{file}", encoding: 'utf-8').grep(/translated: true/).any?
+    #         puts "#{file} requires auto translation!"
+    #         input_file = File.read("docs/#{file}", encoding: 'utf-8')
 
-            ## directly from Google Cloud Translate
-            response = client.translate_text input_file.split(/\n(?=[^\n])/), locale, parent, source_language_code: "es", mime_type:"text/plain"
-            output = response.translations.reduce('') { |acc, t| acc.concat(t.translated_text).concat("\n") }.chomp
+    #         ## directly from Google Cloud Translate
+    #         response = client.translate_text input_file.split(/\n(?=[^\n])/), locale, parent, source_language_code: "es", mime_type:"text/plain"
+    #         output = response.translations.reduce('') { |acc, t| acc.concat(t.translated_text).concat("\n") }.chomp
 
-            ## Writing to translated file
-            file = File.open("docs/#{locale}/#{file}", "w")
+    #         ## Writing to translated file
+    #         file = File.open("docs/#{locale}/#{file}", "w")
 
-            #GCloudTranslate Patches  [issue ref: https://issuetracker.google.com/issues/119256504]
-            # Markdown bold
-            output.scan(/\*\*[^\*]+\*\*/).each { |bold| output.gsub!(bold, bold.gsub(/ *\*\* */,'**')) }
-            # Links
-            output.gsub!(/(?<=[\!]) (?=[\[])|(?<=[\]]) (?=[\(])|(?<=[\/]) (?=.)| (?=[\/])/,'')
-            # code snippets wrongly translated (tips, ``)
-            output.gsub!('::: type Type', ':::tip Tip')
-            output.gsub!(/(?<=^|[^`])``(?=[^`]|$)/, '```')
-            # html attrs
-            output.gsub!(/(?<=src|width|style|) = /, '=')
+    #         #GCloudTranslate Patches  [issue ref: https://issuetracker.google.com/issues/119256504]
+    #         # Markdown bold
+    #         output.scan(/\*\*[^\*]+\*\*/).each { |bold| output.gsub!(bold, bold.gsub(/ *\*\* */,'**')) }
+    #         # Links
+    #         output.gsub!(/(?<=[\!]) (?=[\[])|(?<=[\]]) (?=[\(])|(?<=[\/]) (?=.)| (?=[\/])/,'')
+    #         # code snippets wrongly translated (tips, ``)
+    #         output.gsub!('::: type Type', ':::tip Tip')
+    #         output.gsub!(/(?<=^|[^`])``(?=[^`]|$)/, '```')
+    #         # html attrs
+    #         output.gsub!(/(?<=src|width|style|) = /, '=')
             
-            file.puts output
-            file.close
-            sleep 15
-        end
-    end
+    #         file.puts output
+    #         file.close
+    #         sleep 15
+    #     end
+    # end
 end
