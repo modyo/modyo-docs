043056ce8d1c103296a3a05fb56c5d08663879dc
Daniel Garza <dgarza@modyo.com>
2021-12-01 10:15:39 -0600

    Nuevo home en master

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     "/es/": {
       lang: "es",
-      title: "Documentación de Modyo",
+      title: "Modyo Docs",
       description: "Bienvenido a la documentación oficial de Modyo.",
     },
   },
@@ -60,7 +60,7 @@ module.exports = {
                 "/en/platform/release-notes",
                 "/en/platform/training",
               ],
-            },
+            },    
             {
               title: "Tutorials",
               path: "/en/platform/tutorials/",
@@ -70,8 +70,6 @@ module.exports = {
                 "/en/platform/tutorials/how-to-create-home",
                 "/en/platform/tutorials/how-to-create-a-custom-view",
                 "/en/platform/tutorials/how-to-create-private-site",
-                "/en/platform/tutorials/how-to-create-a-menu",
-                "/en/platform/tutorials/how-to-create-a-pwa",
               ],
             },
             {
@@ -133,7 +131,7 @@ module.exports = {
                 "/en/platform/insights/digital-factory",
                 ["/en/platform/insights/data-studio", "Data Studio"],
               ],
-            },
+            },            
           ],
           "/en/widgets/": [
             {
@@ -170,7 +168,6 @@ module.exports = {
               collapsable: true,
               children: [
                 "/en/widgets/investments/accounts",
-                "/en/widgets/investments/accounts-react",
                 "/en/widgets/investments/general-summary",
                 "/en/widgets/investments/detailed-summary",
                 "/en/widgets/investments/investments-transfers",
@@ -219,10 +216,10 @@ module.exports = {
               title: "Legal",
               collapsable: true,
               children: [
-                ["/en/legal/", "License Agreement"],
+                ["/en/legal/", "License Agreement"],                
               ],
-            },
-          ],
+            },            
+          ],                  
         },
       },
       "/es/": {
@@ -243,7 +240,7 @@ module.exports = {
               collapsable: true,
               children: [
                 ["/es/platform/whats-new", "¿Qué hay de nuevo?"],
-                ["/es/platform/key-concepts", "Conceptos claves"],
+                ["/es/platform/key-concepts", "Conceptos claves"],    
                 "/es/platform/release-notes",
                 ["/es/platform/training", "Entrenamiento"],
               ],
@@ -258,11 +255,12 @@ module.exports = {
                 "/es/platform/tutorials/how-to-create-a-custom-view",
                 "/es/platform/tutorials/how-to-create-private-site",
                 "/es/platform/tutorials/how-to-create-a-menu",
+                "/es/platform/tutorials/conect-modyo-saleforce",
                 "/es/platform/tutorials/how-to-create-a-pwa",
               ],
             },
             {
-              title: "Plataforma",
+              title: "Plataforma", 
               path: "/es/platform/core/",
               collapsable: true,
               children: [
@@ -322,7 +320,7 @@ module.exports = {
                 "/es/platform/insights/digital-factory",
                 ["/es/platform/insights/data-studio", "Data Studio"],
               ],
-            },
+            },          
           ],
           "/es/widgets/": [
             {
@@ -360,7 +358,6 @@ module.exports = {
               collapsable: true,
               children: [
                 "/es/widgets/investments/accounts",
-                "/es/widgets/investments/accounts-react",
                 "/es/widgets/investments/general-summary",
                 "/es/widgets/investments/detailed-summary",
                 "/es/widgets/investments/investments-transfers",
@@ -389,7 +386,7 @@ module.exports = {
                 "/es/widgets/insurance-retail/quote",
                 "/es/widgets/insurance-retail/detailed-view",
               ],
-            },
+            },           
           ],
           "/es/services/": [
             {
@@ -401,7 +398,7 @@ module.exports = {
                 "/es/services/connect",
                 "/es/services/faq",
               ],
-            },
+            },           
           ],
           "/es/brand/": [
             {
@@ -414,16 +411,16 @@ module.exports = {
                 "/es/brand/typography",
                 "/es/brand/illustrations",
               ],
-            },
+            },           
           ],
           "/es/legal/": [
             {
               title: "Legal",
               collapsable: true,
               children: [
-                ["/es/legal/", "Acuerdo de Licencia"],
+                ["/es/legal/", "Acuerdo de Licencia"],                
               ],
-            },
+            },            
           ],
         },
       },
