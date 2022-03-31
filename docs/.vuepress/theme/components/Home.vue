f339cafc86ce36680d581c5a5353069ee049f790
Daniel Garza <dgarza@modyo.com>
2022-03-31 11:34:41 -0600

    Agregar Google Tag Manager a Home

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <main id="test-deployment-01" class="home" aria-labelledby="main-title">
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MW9VKMC');
+    </script>
     <div class=" m-0 p-0">
       <div class="bg-dark">
         <div class="container pb-0 pb-md-5 pt-5 mt-5 bg-hero">
