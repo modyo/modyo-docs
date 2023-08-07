4dadfd2ad3cd81edfb46bb16eb0c7b893b67b9b2
fabian rm <friquelme@modyo.com>
2023-08-07 14:27:52 -0400

    #602 agregar documentacion estatica y primeros json con documentacion a docs

---
+++ docs/.vuepress/public/swagger/swagger-initializer.js
@@ -0,0 +1,15 @@
+function swaggerInit(url) {
+  window.ui = SwaggerUIBundle({
+    url,
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+}
